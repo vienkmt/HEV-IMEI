173d2c240d7dcddbac6d470fa162e493cd362d3c
VienPc <vienkmt@hotmail.com>
2020-11-19 11:21:42 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HEV_Agent_V2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            bool createdNew;
+
+            Mutex m = new Mutex(true, "HEV_FI_AGENT", out createdNew);
+            if (!createdNew)
+            {
+                // myApp is already running...
+                MessageBox.Show("Phần mềm đang chạy, vào Task Manger - Tìm HEV_IMEI_AGENT tắt trước đi đã");
+                //Application.Restart();
+                return;
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmAgentScrew());
+        }
+    }
+}
