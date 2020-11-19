e5793897107630b5bb9e1601f710e3bb4356bb24
VienPc <vienkmt@hotmail.com>
2020-11-19 11:22:02 +0700

    HEV IMEI OK

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace HEV_Agent_V2
             Mutex m = new Mutex(true, "HEV_FI_AGENT", out createdNew);
             if (!createdNew)
             {
-                // myApp is already running...
+                // myApp is already running....
                 MessageBox.Show("Phần mềm đang chạy, vào Task Manger - Tìm HEV_IMEI_AGENT tắt trước đi đã");
                 //Application.Restart();
                 return;
